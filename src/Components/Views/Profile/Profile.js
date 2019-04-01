f3d9ff3f636e56bb64ca9aef7c090073780eb40d
Steve Sarnecki <svs5595@rit.edu>
2019-04-01 13:01:10 -0400

    Profile init styling complete

--- src/Components/Views/Profile/Profile.js
+++ src/Components/Views/Profile/Profile.js
@@ -17,6 +17,34 @@ const ScProfile = styled.div`
     margin-top: 4%;
 `;
 
+//Styling for the profile elements
+const ScProInfo = styled.div`
+    margin: auto;
+    display: block;
+    text-align: center;
+    width: 100%;
+    margin-left: 25%;
+    margin-top: 4%;
+    font-size: 18pt;
+`;
+
+const ScProInfoField = styled.p`
+    display: block;
+    padding: 5px;
+    margin-top: 20px;
+    margin-bot: 20px;
+    font-size: 18pt;
+`;
+
+const ScProEditButton = styled.button`
+    display: block;
+    padding: 5px;
+    margin-top: 20px;
+    margin-bot: 20px;
+    font-size: 18pt;
+`;
+
+//Styling for the form elements
 const ScProForm = styled.form`
     margin: auto;
     display: block;
@@ -54,12 +82,13 @@ class Profile extends Component {
             nfavFood: '',
             favRest: '',
             blank: true,
+            usernameCheck : false,
         }
     }
 
     //Handle Change of User's Name
     handleNameChange = e => {
-        this.setState({ name: e.target.value });
+        this.setState({ name: e.target.value});
     }
 
     //Handle Change of User's Allergy
@@ -82,6 +111,11 @@ class Profile extends Component {
         this.setState({ favRest: e.target.value });
     }
 
+    //Handle Submission of the inital user data form
+    handleInitSave = e => {
+        this.setState({ blank: false, usernameCheck: true })
+    }
+
     //Handle Submission of the user data form
     handleSave = e => {
         this.setState({ blank: false });
@@ -94,27 +128,48 @@ class Profile extends Component {
 
     render() {
         if(this.state.blank == true){
-            //If no user data exists, render form to be submitted
-            return (
-                <div>
-                    <ScProForm>
-                        <ScProField type="text" name="name" placeholder="Username" value={this.state.symbol} onChange={this.handleNameChange}/>
-                        <ScProField type="text" name="allergy" placeholder="Allergies" value={this.state.allergy} onChange={this.handleAllergyChange}/>
-                        <ScProField type="text" name="favFood" placeholder="Favorite Foods" value={this.state.favFood} onChange={this.handleFavFoodChange}/>
-                        <ScProField type="text" name="nfavFood" placeholder="Hated Foods" value={this.state.nfavFood} onChange={this.handleNFavFoodChange}/>
-                        <ScProField type="text" name="favRest" placeholder="Favorite Restauranut" value={this.state.favRest} onChange={this.handleRestChange}/>
-                        <ScProSubmitButton type="button" onClick={this.handleSave}>Save</ScProSubmitButton>
-                    </ScProForm>
-                </div>
-            );
+            if(this.state.usernameCheck == false){
+                //If no user data exists, render form to be submitted
+                return (
+                    <div>
+                        <ScProForm>
+                            <ScProField type="text" name="name" placeholder="Username" value={this.state.symbol} onChange={this.handleNameChange}/>
+                            <ScProField type="text" name="allergy" placeholder="Allergies" value={this.state.allergy} onChange={this.handleAllergyChange}/>
+                            <ScProField type="text" name="favFood" placeholder="Favorite Foods" value={this.state.favFood} onChange={this.handleFavFoodChange}/>
+                            <ScProField type="text" name="nfavFood" placeholder="Hated Foods" value={this.state.nfavFood} onChange={this.handleNFavFoodChange}/>
+                            <ScProField type="text" name="favRest" placeholder="Favorite Restauranut" value={this.state.favRest} onChange={this.handleRestChange}/>
+                            <ScProSubmitButton type="button" onClick={this.handleInitSave}>Save</ScProSubmitButton>
+                        </ScProForm>
+                    </div>
+                );
+            }
+            else{
+                //If no user data exists, render form to be submitted
+                return (
+                    <div>
+                        <ScProForm>
+                            <ScProInfoField>Username: {this.state.name}</ScProInfoField>
+                            <ScProField type="text" name="allergy" placeholder="Allergies" value={this.state.allergy} onChange={this.handleAllergyChange}/>
+                            <ScProField type="text" name="favFood" placeholder="Favorite Foods" value={this.state.favFood} onChange={this.handleFavFoodChange}/>
+                            <ScProField type="text" name="nfavFood" placeholder="Hated Foods" value={this.state.nfavFood} onChange={this.handleNFavFoodChange}/>
+                            <ScProField type="text" name="favRest" placeholder="Favorite Restauranut" value={this.state.favRest} onChange={this.handleRestChange}/>
+                            <ScProSubmitButton type="button" onClick={this.handleSave}>Save</ScProSubmitButton>
+                        </ScProForm>
+                    </div>
+                );
+            }
         }
         else{
             //If user data exists render it with option to edit, editing will set blank to true
             return (
-                <div>
-                    <p>oof</p>
-                    <button type = "button" onClick={this.handleEdit}>Edit</button>
-                </div>
+                <ScProInfo>
+                    <ScProInfoField>Username: {this.state.name}</ScProInfoField>
+                    <ScProInfoField>Allergies: {this.state.allergy}</ScProInfoField>
+                    <ScProInfoField>Favorite Foods: {this.state.favFood}</ScProInfoField>
+                    <ScProInfoField>Disliked Foods: {this.state.nfavFood}</ScProInfoField>
+                    <ScProInfoField>Favorite Restauranut: {this.state.favRest}</ScProInfoField>
+                    <ScProEditButton type = "button" onClick={this.handleEdit}>Edit</ScProEditButton>
+                </ScProInfo>
             );
         }
     }
