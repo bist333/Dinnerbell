f3f4255fd706557762cf6c135a1b7546105bc198
pdfkpb <pdfkpb@gmail.com>
2019-04-03 12:09:53 -0400

    working on pending meals boxes

--- src/Components/Views/Meals/Meals.js
+++ src/Components/Views/Meals/Meals.js
@@ -6,12 +6,32 @@ import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 
 const ScMeals = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     text-align: center;
 `;
 
 const ScMealBox = styled.div`
+    text-align: center;
+    padding: 5%;
+    width: 25%;
+    height 30px;
+    border-style: solid;
+    border-color: 2px;
+    cursor: pointer;
+    background-color: #EF476F;
+    font-size: 18px;
+    font-weight: bold;
+    color: #F8F8FF;
+    border-radius: 10px;
 `;
 
+const ScAcceptButton = styled.button`
+    background-color: #4CAF50;
+    width: 10%;
+    height: 10%;
+`;
 
 class Meals extends Component {
     constructor(props) {
@@ -24,7 +44,7 @@ class Meals extends Component {
     render() {
         return (
             <ScMeals>
-                {this.state.pendingMeals}
+                {this.state.pendingMeals.map(meal => <ScMealBox>{meal.meal}<br />{meal.type}<br />{meal.time}<br /><ScAcceptButton></ScAcceptButton></ScMealBox>)}
             </ScMeals>
         );
     }
--- src/Components/Views/Payments/Payments.js
+++ src/Components/Views/Payments/Payments.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 const ScPayments = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     text-align: center;
+`;
+
+const ScPaymentBox = styled.ul`
+    text-align: center;
+    padding: 10%;
+    width: 25%;
+    border-style: solid;
+    border-color: 2px;
+    padding-bottom: 30px;
+    cursor: pointer;
+    background-color: #EF476F;
+    font-size: 18px;
+    font-weight: bold;
+    color: #F8F8FF;
+    border-radius: 10px;
 `;
 
 
 class Payments extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pendingPayments: cookies.get('persisted_data').pending_payments
+        };
+    }
+
     render() {
         return (
             <ScPayments>
-                This is $$$$!
+                {this.state.pendingPayments.map(meal => <ScPaymentBox><li>{meal.meal}</li><li>{meal.owed}</li><li>{meal.time}</li></ScPaymentBox>)}
             </ScPayments>
         );
     }
