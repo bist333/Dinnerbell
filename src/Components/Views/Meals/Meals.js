180411ae50b52f14a6599145515689da735a8abe
pdfkpb <pdfkpb@gmail.com>
2019-04-03 11:31:20 -0400

    starting pending meals

--- src/Components/Views/Meals/Meals.js
+++ src/Components/Views/Meals/Meals.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 const ScMeals = styled.div`
     text-align: center;
 `;
 
+const ScMealBox = styled.div`
+`;
+
 
 class Meals extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pendingMeals: cookies.get('persisted_data').pending_meals
+        };
+    }
+    
     render() {
         return (
             <ScMeals>
-                This is a meals!!!
+                {this.state.pendingMeals}
             </ScMeals>
         );
     }
